893ca744907993b2af44ed935be796edb3abc5b3
Bradley <brad.driver33@gmail.com>
2018-12-11 13:45:52 -0500

    coded the middleware for the authentication request and the middleware that recieves the data from google and runs the function on strategy config.

--- authorization/auth.js
+++ authorization/auth.js
@@ -64,3 +64,13 @@ function isUserAuthenticated(req, res, next) {
 app.get('/', (req, res) => {
     res.render('auth.html');
 });
+
+// passport.authenticate middleware is used here to authenticate the request
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile'] // Used to specify the required data
+}));
+
+// The middleware receives the data from Google and runs the function on Strategy config
+app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/secret');
+});
