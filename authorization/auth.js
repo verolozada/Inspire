0b1aca2cddc44c59f2f1b7c41f36534e51bd3a2c
Bradley <brad.driver33@gmail.com>
2018-12-11 13:42:13 -0500

    added code to put information into a cookie and code to decode the recieved cookie and persist session.

--- authorization/auth.js
+++ authorization/auth.js
@@ -40,3 +40,13 @@ passport.use(new GoogleStrategy({
         done(null, profile); // passes the profile data to serializeUser
     }
 ));
+
+// Used to stuff a piece of information into a cookie
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+// Used to decode the received cookie and persist session
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
